8489d5a50fa484822aeff306bada03aa2a400d9f
ljylhm <948021695@qq.com>
2019-03-05 14:20:22 +0800

    # twoSum 第二种解法

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # leetcode
 这段时间开始做leetcode的题目了，题目几乎都是用nodeJs完成的，全当是自己的记录吧，有的问题虽然已经通过，但是执行时间很长，希望这些问题也能在后期得到优化，2019，fighting。
+
+## 两数之和
+对于两数之和的题目又有了新的想法，通过空间来换取时间，增加一个对象来映射索引和真实值的关系，然后只需通过一层循环找到对应的两个数。时间复杂度由O(n2)降为O(n) 空间复杂度从O(1)变成O(n)
+
+```javascript
+    var twoSum1 = function (nums, target) {
+    let mapObj = {}
+    let res = []
+    let len = nums.length
+    nums.forEach((e, i) => {
+        mapObj[e] = i
+    });
+
+    for (let i = 0; i < len; i++) {
+        let j = mapObj[target - nums[i]]
+        if (j && j != i) {
+            res = i < j ? [i, j] : [j, i]
+        }
+    }
+    return res
+}
+```
--- 两数之和/main.js
+++ 两数之和/main.js
@@ -4,6 +4,9 @@
  * @return {number[]}
  */
 // ----------- 执行效率为120ms -----------
+/**
+ * 暴力破解法 时间复杂度为O(n2) 空间复杂度为O(1)
+ */
 var twoSum = function (nums, target) {
     for (var i = 0, len = nums.length; i < len; i++) {
 
@@ -15,4 +18,26 @@ var twoSum = function (nums, target) {
 
     }
 };
+
+/** 第二种做法增加了一个对象 在对象中获取某个值得复杂度为o(1)
+ *  不过增加了空间复杂度 空间复杂度为o(n)
+ *  时间复杂度为o(n)
+ */
+
+var twoSum1 = function (nums, target) {
+    let mapObj = {}
+    let res = []
+    let len = nums.length
+    nums.forEach((e, i) => {
+        mapObj[e] = i
+    });
+
+    for (let i = 0; i < len; i++) {
+        let j = mapObj[target - nums[i]]
+        if (j && j != i) {
+            res = i < j ? [i, j] : [j, i]
+        }
+    }
+    return res
+}
 
